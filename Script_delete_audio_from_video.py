546a83a8002dc7aebbb18f438c142d9bf5c374e2
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2024-11-26 15:05:46 -0500

    code to strip sound

--- Script_delete_audio_from_video.py
+++ Script_delete_audio_from_video.py
@@ -1,10 +1,10 @@
 from moviepy.editor import VideoFileClip
 
-videoclip = VideoFileClip("https://raw.githubusercontent.com/jsaraviadrago/sound-data/blob/main/chess_rust.mkv") # video example
+videoclip = VideoFileClip("/home/juan-carlos/Vídeos/chess_rust.mkv") # video example
 new_clip = videoclip.without_audio() # strip audio
 
 # Put any folder you want
-new_clip.write_videfile("/home/juan-carlos/Vídeos/final_chess_clip.mp4")
+new_clip.write_videofile("/home/juan-carlos/Vídeos/final_chess_clip.mp4")
 
 
 
